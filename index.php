b050ada46c7283d6dc2f616959699e5cca05bd19
Branko Ajzele <ajzelegmail.com>
2016-02-03 12:03:05 +0100

    Sample data code

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php
-
-/**
- * @todo Study include vs. require, *_once
- */
-require_once 'config.php';
-
-echo 'Hello!';
+<?php require_once 'config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Web Shop App</title>
+</head>
+<body>
+<h1>Web Shop App</h1>
+<div class="category-menu">
+    <ul>
+        <li>Category 1</li>
+        <li>Category 2</li>
+    </ul>
+</div>
+</body>
+</html>
